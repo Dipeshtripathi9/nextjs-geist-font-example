3b1f8e2693a2149395d2e39ed6cb0939613d4227
Dipeshtripathi9 <dipeshtripathi01@gmail.com>
2025-07-21 18:43:02 +0530

    Add src/app/dashboard/layout.tsx 1753103580978

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="flex h-screen">
+        {children}
+      </div>
+    </div>
+  )
+}
